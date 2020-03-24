751f8dfe8543c0d37a923fb7d3e81df655f98a5b
winnieinyourarea <thepiggywin2000@gmail.com>
2020-03-24 12:16:21 +0700

    Updates

---
+++ precache-manifest.4c0adb245bad59660182b4815ad55fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f79837515e5466d95a6d6de1e16a7c",
+    "url": "/lab8-expenses/index.html"
+  },
+  {
+    "revision": "fd8127d165e6e271173d",
+    "url": "/lab8-expenses/static/css/main.4851e9fc.chunk.css"
+  },
+  {
+    "revision": "cd7fd592ac5f72da261c",
+    "url": "/lab8-expenses/static/js/2.9a322848.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/lab8-expenses/static/js/2.9a322848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8127d165e6e271173d",
+    "url": "/lab8-expenses/static/js/main.eec76317.chunk.js"
+  },
+  {
+    "revision": "036a8bae09ead8fcaa98",
+    "url": "/lab8-expenses/static/js/runtime-main.ff6757b9.js"
+  }
+]);
